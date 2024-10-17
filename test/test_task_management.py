ba288e6cce2917def36a5df1b1b1f5d0a76dac60
Aditya Batgeri <adityabatgeri@gmail.com>
2024-10-17 21:26:36 +0530

    final

--- test/test_task_management.py
+++ test/test_task_management.py
@@ -3,13 +3,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-# Specify the path to ChromeDriver
 service = Service('/usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=service)
 
-# Test Scenario 1: Create a Task
 def test_create_task():
-    driver.get("http://localhost:3000")  # Assuming your client runs on localhost:3000
+    driver.get("http://localhost:3000") 
 
     title_field = driver.find_element(By.NAME, "title")
     description_field = driver.find_element(By.NAME, "description")
@@ -21,7 +19,6 @@ def test_create_task():
 
     time.sleep(2)
 
-# Running the test
 test_create_task()
 
 driver.quit()
